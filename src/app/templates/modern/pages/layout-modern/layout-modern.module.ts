0ea5a06ce3e18450f11634b9ba452c0d515ae66b
Juan José Arteta <77931977+jj131204@users.noreply.github.com>
2024-07-26 00:10:51 -0500

    home moderno con responsive

--- src/app/templates/modern/pages/layout-modern/layout-modern.module.ts
+++ src/app/templates/modern/pages/layout-modern/layout-modern.module.ts
@@ -5,6 +5,12 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { RouterModule } from '@angular/router';
+import { WeAreComponent } from '../home-modern/weAre/weAre.component';
+import { StatisticsComponent } from '../home-modern/statistics/statistics.component';
+import { ServicesComponent } from '../home-modern/services/services.component';
+import { PortafolioComponent } from '../home-modern/portafolio/portafolio.component';
+import { EventsComponent } from '../home-modern/events/events.component';
+import { OurTeamComponent } from '../home-modern/our-team/our-team.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +18,12 @@ import { RouterModule } from '@angular/router';
     HeaderComponent,
     FooterComponent,
     CarouselComponent,
+    WeAreComponent,
+    StatisticsComponent,
+    ServicesComponent,
+    PortafolioComponent,
+    EventsComponent,
+    OurTeamComponent
   ],
   imports: [CommonModule, RouterModule],
   exports: [LayoutModernComponent],
