1d2682e65183be0306f8d76cb9548cbcdcb8855a
SebastianContreras <90106481@grupovanti.com>
2024-07-28 12:24:19 -0500

    feat: update all

--- src/app/templates/minimalist/pages/home-minimalist/our-team/our-team.component.ts
+++ src/app/templates/minimalist/pages/home-minimalist/our-team/our-team.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { Subject, Observable, takeUntil } from 'rxjs';
+import { ITeam } from '../../../../../core/interfaces/web.interface';
+import { WebState } from '../../../../../core/store/web/web.state';
 
 @Component({
   selector: 'app-our-team',
   templateUrl: './our-team.component.html',
   styleUrls: ['./our-team.component.scss']
 })
-export class OurTeamComponent implements OnInit {
 
-  constructor() { }
+export class OurTeamComponent implements OnInit, OnDestroy {
+  private destroy: Subject<boolean> = new Subject<boolean>();
+  teamData$: Observable<ITeam> = new Observable();
 
-  ngOnInit() {
+  teamData: ITeam;
+
+  constructor(private store: Store) {
+    this.teamData$ = this.store.select(WebState.teamData);
   }
 
+  ngOnInit() {
+    this.subscribeState();
+  }
+
+  subscribeState() {
+    this.teamData$.pipe(takeUntil(this.destroy)).subscribe((resp) => {
+      this.teamData = resp;
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroy.next(true);
+    this.destroy.unsubscribe();
+  }
 }
