58203191b634bd5f82a38c397786473c6efffb81
SebastianContreras <90106481@grupovanti.com>
2024-08-11 15:19:20 -0500

    feat: team

--- src/app/templates/modern/pages/home-modern/events/events.component.ts
+++ src/app/templates/modern/pages/home-modern/events/events.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { Subject, Observable, takeUntil } from 'rxjs';
+import { IEvent, IEventsSection } from '../../../../../core/interfaces/events.interface';
+import { EventsState } from '../../../../../core/store/events/events.state';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.scss']
 })
-export class EventsComponent implements OnInit {
 
-  constructor() { }
+export class EventsComponent implements OnInit, OnDestroy {
+  private destroy: Subject<boolean> = new Subject<boolean>();
+  listEvents$: Observable<IEvent[]> = new Observable();
+  sectionEvents$: Observable<IEventsSection> = new Observable();
 
-  ngOnInit() {
+  listEvents: IEvent[];
+  sectionEvents: IEventsSection;
+
+  constructor(private store: Store) {
+    this.listEvents$ = this.store.select(EventsState.ListAllEvents);
+    this.sectionEvents$ = this.store.select(EventsState.EventsSection);
   }
 
+  ngOnInit() {
+    this.subscribeState();
+  }
+
+  subscribeState() {
+    this.listEvents$.pipe(takeUntil(this.destroy)).subscribe((resp) => {
+      this.listEvents = resp;
+    });
+
+    this.sectionEvents$.pipe(takeUntil(this.destroy)).subscribe((resp) => {
+      this.sectionEvents = resp;
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroy.next(true);
+    this.destroy.unsubscribe();
+  }
 }
--- src/app/templates/modern/pages/home-modern/services/services.component.ts
+++ src/app/templates/modern/pages/home-modern/services/services.component.ts
@@ -1,16 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { Subject, Observable, takeUntil } from 'rxjs';
+import {
+  IService,
+  IServiceSection,
+} from '../../../../../core/interfaces/services.interface';
+import { ServicesState } from '../../../../../core/store/services/services.state';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
-  styleUrls: ['./services.component.scss']
+  styleUrls: ['./services.component.scss'],
 })
 
-export class ServicesComponent implements OnInit {
+export class ServicesComponent implements OnInit, OnDestroy {
+  private destroy: Subject<boolean> = new Subject<boolean>();
+  listServices$: Observable<IService[]> = new Observable();
+  sectionServices$: Observable<IServiceSection> = new Observable();
 
-  constructor() { }
+  listServices: IService[];
+  sectionServices: IServiceSection;
 
-  ngOnInit() {
+  constructor(private store: Store) {
+    this.listServices$ = this.store.select(ServicesState.ListAllServices);
+    this.sectionServices$ = this.store.select(ServicesState.ServiceSection);
   }
 
+  ngOnInit() {
+    this.subscribeState();
+  }
+
+  subscribeState() {
+    this.listServices$.pipe(takeUntil(this.destroy)).subscribe((resp) => {
+      this.listServices = resp;
+    });
+
+    this.sectionServices$.pipe(takeUntil(this.destroy)).subscribe((resp) => {
+      this.sectionServices = resp;
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroy.next(true);
+    this.destroy.unsubscribe();
+  }
 }
