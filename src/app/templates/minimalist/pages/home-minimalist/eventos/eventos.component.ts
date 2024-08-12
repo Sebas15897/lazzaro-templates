10a4a43b14a89bcaa3f0a7f540f72c59df2598c0
SebastianContreras <90106481@grupovanti.com>
2024-08-12 08:31:50 -0500

    update all

--- src/app/templates/minimalist/pages/home-minimalist/eventos/eventos.component.ts
+++ src/app/templates/minimalist/pages/home-minimalist/eventos/eventos.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngxs/store';
+import { Subject, Observable, takeUntil } from 'rxjs';
+import { IEvent, IEventsSection } from '../../../../../core/interfaces/events.interface';
+import { SelectEventAction } from '../../../../../core/store/events/events.actions';
+import { EventsState } from '../../../../../core/store/events/events.state';
 
 @Component({
   selector: 'app-eventos',
@@ -6,11 +12,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss']
 })
 
-export class EventosComponent implements OnInit {
+export class EventosComponent implements OnInit, OnDestroy {
+  private destroy: Subject<boolean> = new Subject<boolean>();
+  listEvents$: Observable<IEvent[]> = new Observable();
+  sectionEvents$: Observable<IEventsSection> = new Observable();
 
-  constructor() { }
+  listEvents: IEvent[];
+  sectionEvents: IEventsSection;
 
-  ngOnInit() {
+  constructor(private store: Store, private router: Router) {
+    this.listEvents$ = this.store.select(EventsState.ListAllEvents);
+    this.sectionEvents$ = this.store.select(EventsState.EventsSection);
   }
 
+  ngOnInit() {
+    this.subscribeState();
+  }
+
+  subscribeState() {
+    this.listEvents$.pipe(takeUntil(this.destroy)).subscribe((resp) => {
+      this.listEvents = resp;
+    });
+
+    this.sectionEvents$.pipe(takeUntil(this.destroy)).subscribe((resp) => {
+      this.sectionEvents = resp;
+    });
+  }
+
+  onSelectEvent(event: IEvent) {
+    this.store
+      .dispatch(new SelectEventAction(event))
+      .pipe(takeUntil(this.destroy))
+      .subscribe(() => {
+        this.router.navigate(['/clasic/events']);
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy.next(true);
+    this.destroy.unsubscribe();
+  }
 }
--- src/app/templates/minimalist/pages/home-minimalist/portafolio/portafolio.component.ts
+++ src/app/templates/minimalist/pages/home-minimalist/portafolio/portafolio.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngxs/store';
+import { Subject, Observable, takeUntil } from 'rxjs';
+import { IProject, IPortfolioSection } from '../../../../../core/interfaces/portfolio.iterface';
+import { SelectProjectAction } from '../../../../../core/store/portfolio/portfolio.actions';
+import { PortfolioState } from '../../../../../core/store/portfolio/portfolio.state';
 
 @Component({
   selector: 'app-portafolio',
   templateUrl: './portafolio.component.html',
   styleUrls: ['./portafolio.component.scss']
 })
-export class PortafolioComponent implements OnInit {
 
-  constructor() { }
+export class PortafolioComponent implements OnInit, OnDestroy {
+  private destroy: Subject<boolean> = new Subject<boolean>();
+  listProjects$: Observable<IProject[]> = new Observable();
+  sectionProjects$: Observable<IPortfolioSection> = new Observable();
 
-  ngOnInit() {
+  listProjects: IProject[];
+  sectionProjects: IPortfolioSection;
+
+  constructor(private store: Store, private router: Router) {
+    this.listProjects$ = this.store.select(PortfolioState.ListAllProjects);
+    this.sectionProjects$ = this.store.select(PortfolioState.PortfolioSection);
   }
 
+  ngOnInit() {
+    this.subscribeState();
+  }
+
+  subscribeState() {
+    this.listProjects$.pipe(takeUntil(this.destroy)).subscribe((resp) => {
+      this.listProjects = resp;
+    });
+
+    this.sectionProjects$.pipe(takeUntil(this.destroy)).subscribe((resp) => {
+      this.sectionProjects = resp;
+    });
+  }
+
+  onSelectProject(project: IProject) {
+    this.store
+      .dispatch(new SelectProjectAction(project))
+      .pipe(takeUntil(this.destroy))
+      .subscribe(() => {
+        this.router.navigate(['/clasic/portafolio']);
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy.next(true);
+    this.destroy.unsubscribe();
+  }
 }
