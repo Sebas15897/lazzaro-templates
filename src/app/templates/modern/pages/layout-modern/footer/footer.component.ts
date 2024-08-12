88033e0b2f0ef54d72772ccea54f66faf014ab92
SebastianContreras <90106481@grupovanti.com>
2024-08-11 21:29:44 -0500

    feat: uodatea ll

--- src/app/templates/modern/pages/layout-modern/footer/footer.component.ts
+++ src/app/templates/modern/pages/layout-modern/footer/footer.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Store } from '@ngxs/store';
+import { Subject, Observable, takeUntil } from 'rxjs';
+import { IFooter, IStyle } from '../../../../../core/interfaces/web.interface';
+import { WebState } from '../../../../../core/store/web/web.state';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
 
+export class FooterComponent implements OnInit, OnDestroy {
+  private destroy: Subject<boolean> = new Subject<boolean>();
+  footer$: Observable<IFooter> = new Observable();
+  styles$: Observable<IStyle> = new Observable();
+
+  footer: IFooter;
+
+  styles: IStyle;
+
+  constructor(
+    private store: Store,
+    private viewportScroller: ViewportScroller,
+    private router: Router
+  ) {
+    this.footer$ = this.store.select(WebState.footer);
+    this.styles$ = this.store.select(WebState.styleData);
+
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const tree = this.router.parseUrl(this.router.url);
+        if (tree.fragment) {
+          this.viewportScroller.scrollToAnchor(tree.fragment);
+        }
+      }
+    });
+  }
+
+  ngOnInit() {
+    this.subscribeState();
+  }
+
+  subscribeState() {
+    this.footer$.pipe(takeUntil(this.destroy)).subscribe((resp) => {
+      if (resp) {
+        this.footer = resp;
+      } else {
+        this.footer = null;
+      }
+    });
+
+    this.styles$.pipe(takeUntil(this.destroy)).subscribe((resp) => {
+      if (resp) {
+        this.styles = resp;
+      } else {
+        this.styles = null;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroy.next(true);
+    this.destroy.unsubscribe();
+  }
 }
