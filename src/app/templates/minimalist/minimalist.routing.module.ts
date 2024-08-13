0c532b523717a714b9cecf39be1e16b40a894752
SebastianContreras <90106481@grupovanti.com>
2024-08-13 02:22:48 -0500

    update all

--- src/app/templates/minimalist/minimalist.routing.module.ts
+++ src/app/templates/minimalist/minimalist.routing.module.ts
@@ -1,6 +1,8 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { LayoutMinimalistComponent } from './pages/layout-minimalist/layout-minimalist.component';
+import { TermsConditionsComponent } from '../../shared/terms-conditions/terms-conditions.component';
+import { TransparencyComponent } from '../../shared/transparency/transparency.component';
 
 export const routes: Routes = [
   {
@@ -56,6 +58,14 @@ export const routes: Routes = [
           import('./pages/events-minimalist/events-minimalist.module').then(
             (module) => module.EventsMinimalistModule
           ),
+      },
+      {
+        path: 'terms',
+        component: TermsConditionsComponent,
+      },
+      {
+        path: 'transparency',
+        component: TransparencyComponent,
       },
     ],
   },
