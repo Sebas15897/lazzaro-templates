e9097cc60683abc30871ba48e93973b0468c5311
Juan José Arteta <77931977+jj131204@users.noreply.github.com>
2024-08-26 12:27:48 -0500

    fix: style our team

--- src/app/templates/minimalist/pages/home-minimalist/reservas/reservas.component.ts
+++ src/app/templates/minimalist/pages/home-minimalist/reservas/reservas.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { Subject, Observable, takeUntil } from 'rxjs';
+import { IBookings } from '../../../../../core/interfaces/web.interface';
+import { WebState } from '../../../../../core/store/web/web.state';
 
 @Component({
   selector: 'app-reservas',
   templateUrl: './reservas.component.html',
   styleUrls: ['./reservas.component.css']
 })
-export class ReservasComponent implements OnInit {
 
-  constructor() { }
+export class ReservasComponent implements OnInit, OnDestroy {
+  private destroy: Subject<boolean> = new Subject<boolean>();
+  reservas$: Observable<IBookings> = new Observable();
 
-  ngOnInit() {
+  reservas: IBookings;
+
+  constructor(private store: Store) {
+    this.reservas$ = this.store.select(WebState.bookings);
   }
 
+  ngOnInit() {
+    this.subscribeState();
+  }
+
+  subscribeState() {
+    this.reservas$.pipe(takeUntil(this.destroy)).subscribe((resp) => {
+      this.reservas = resp;
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroy.next(true);
+    this.destroy.unsubscribe();
+  }
 }
